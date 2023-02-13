b778e2d4f20150eb57d3ce3e0471c57b9efe418e
ChrisStro <christopher.strobel@outlook.de>
2023-02-13 21:26:12 +0100

    add powershell onliner

--- 241022_Docker2Kubernetes/3_k8s_cli/2_deploy_via_cli.sh
+++ 241022_Docker2Kubernetes/3_k8s_cli/2_deploy_via_cli.sh
@@ -1,6 +1,7 @@
 # run once
 #docker run -it --rm --name pwsh mcr.microsoft.com/powershell:latest pwsh # that is easy
 kubectl run -it --rm pwsh --image=mcr.microsoft.com/powershell:latest --restart=Never
+kubectl run -it --rm pwsh --image=mcr.microsoft.com/powershell:latest --restart=Never -- pwsh
 
 # create simple pod
 kubectl run whoami --image=traefik/whoami --port=80 #--expose=true
